895e58c2482cbd687d05d8ed4a05c81fb3e3d147
Janak singh dhami <info@jsdhami.com.np>
2024-02-09 12:13:38 +0545

    adding some features

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Create a user for testing purposes in development and test environments only (not production). 
+# The user will be created with the email  [email protected] and the password password. 
+# The password is hashed using the bcrypt gem, so the password is not stored in the database in plain text.
